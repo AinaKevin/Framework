5d823b2513d0afbb0b5118ff4f0ac0716cf05dc6
AinaKevin <120164497+AinaKevin@users.noreply.github.com>
2023-04-03 05:54:46 +0200

    sprint3

---
+++ Testframework/src/java/etu2022/framework/test/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2022.framework.test;
+
+import etu2022.framework.Url;
+
+/**
+ *
+ * @author kevin
+ */
+public class Test {
+    @Url(url="/huhu")
+    public void hehe(){
+        System.out.println("etu2022.framework.test.Test.hehe()");
+    }
+}
--- framework/src/etu2022/framework/Mapping.java
+++ framework/src/etu2022/framework/Mapping.java
@@ -4,6 +4,11 @@
  */
 package etu2022.framework;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 public class Mapping {
     String className;
     String Methode;
@@ -27,5 +32,50 @@ public class Mapping {
     public Mapping(String className, String Methode) {
         this.setClassName(className);
         this.setMethode(Methode);
-    }   
+    } 
+     public static String[] getClassList(String directoryPath)
+    {
+        File file=new File(directoryPath);
+//        ty zany afaka simplifier-na otran'zao
+//        de filtrena fotsiny le .java avy eo
+        File[] f=file.listFiles();
+        FilenameFilter textFilefilter = new FilenameFilter(){
+            public boolean accept(File dir, String name) {
+//        String lowercaseName = name.toLowerCase();
+                if (name.endsWith(".java")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        String [] listefile=file.list(textFilefilter);
+        for (int i = 0; i < listefile.length; i++) {
+            listefile[i]=listefile[i].split(".java")[0];
+        }
+        return listefile;
+    }
+
+
+public static HashMap<String, Mapping> getMethodsHashMapFromPackage(String packageDirectory,String ObjectPackage) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        HashMap<String,Mapping> mapping=new HashMap<>();
+
+        String[] classes=getClassList(packageDirectory);
+        for (int i = 0; i < classes.length; i++) {
+            System.out.println("name: "+classes[i]);
+            Class<?> tempClass=Class.forName(ObjectPackage+classes[i]);
+            Object obj=tempClass.newInstance();
+            Method [] methods=obj.getClass().getDeclaredMethods();
+            for (int j = 0; j < methods.length; j++) {
+                if(methods[j].isAnnotationPresent(Url.class))
+                {
+                    String url=methods[j].getAnnotation(Url.class).url();
+                    String className=classes[i];
+                    String methodName=methods[j].getName();
+                    mapping.put(url,new Mapping(className,methodName));
+                }
+            }
+        }
+        return mapping;
+    }
 }
